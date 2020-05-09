6feef6d2146a2cad2684eb8ec14601ee8181907b
AbidemiT <abidemis01@gmail.com>
2020-05-09 16:32:44 +0100

    Editted

--- assets/js/main.js
+++ assets/js/main.js
@@ -221,11 +221,11 @@
   });
 })(jQuery);
 
-$("#my-form").submit(function (e) {
-  e.preventDefault();
+// $("#my-form").submit(function (e) {
+//   e.preventDefault();
 
-  var $form = $(this);
-  $.post($form.attr("action"), $form.serialize()).then(function () {
-    alert("Thank you!");
-  });
-});
+//   var $form = $(this);
+//   $.post($form.attr("action"), $form.serialize()).then(function () {
+//     alert("Thank you!");
+//   });
+// });
